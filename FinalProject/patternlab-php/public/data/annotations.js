e880726466fb5b8355374ce788c6b4bd5c58e259
Chris <Topher253@gmail.com>
2014-03-20 18:49:23 -0700

    update header.mustache syntax

--- FinalProject/patternlab-php/public/data/annotations.js
+++ FinalProject/patternlab-php/public/data/annotations.js
@@ -1,109 +1,104 @@
 var comments = {
 "comments" : [
 	{
+		"el": ".header",
+		"title" : "Header",
+		"comment": "The header has *px*, *color* border at the bottom.</p>"
+	},
+	{
 		"el": ".logo",
 		"title" : "Logo",
 		"comment": "The logo image is an jpg file, which ensures that the logo displays crisply even on high resolution displays. It is the logo that's been carried over from the original Hollywood Fitness website.</p>"
 	},
 	{
-		"el": ".header__bg",
-		"title" : "Header background",
-		"comment": "<p>The header background should be *color, tec*.</p>"
-	},
-	{
-		"el": ".global__nav",
-		"title" : "Navigation",
+		"el": ".header__nav__button",
+		"title" : "Global navigation",
 		"comment": "<p>Navigation style changes depending on the viewport size. On a bigger screen, all the navigation buttons are visible. For a amaller screen, they collapse to a single button \"Menu\". When the Menu button is clicked, the whole navigation options slide out from the right hand side.</p><p>When all the navigation buttons are shown, the selected state is indicated by the brand color (red), and the hover state a lightly ligher version of the red.</p>"
 	},
 	{
-		"el": ".body__bg",
-		"title" : "Body background",
-		"comment": "<p>The body background should be *color, tec*.</p>"
-	},
-	{
-		"el": ".hero--image",
+		"el": ".hero__image",
 		"title" : "Hero image",
-		"comment": "<p>The hero image should cover the entire width of the screen.</p>"
+		"comment": "<p>The hero image should cover the entire width of the screen up to 1600px, and contains the hero message.</p>"
 	},
 	{
-		"el": ".hero--message",
+		"el": ".hero__message",
 		"title" : "Hero message",
-		"comment": "<p>The hero message should be *color*, *font size* in *font type*. It should be between *30 words* to *50 words*. .</p>"
+		"comment": "<p>The hero message should be *color*, *font size* in *font type*. It should be between *30 characters* to *50 characters*. .</p>"
 	},
 	{
-		"el": ".page__top--title",
-		"title" : "Page title",
-		"comment": "<p>The page title is located at the top left corner to clearly indicate which page the viewer is on. The title should match the navigation naming, i.e. the tile of the page that you land when you click the \"Stuff\" should be \"Stuff.\"</p>"
-	},
-	{
-		"el": ".page__top--content",
-		"title" : "Page description",
-		"comment": "<p>The page description should be in *gray*, *font size* in *font name*. It should be between *30 words* to *50 words*.</p>"
-	},
-	{
-		"el": ".main--header1",
-		"title" : "Header 1",
-		"comment": "<p>The header 1 is the biggest header used on the main content of the page. It should be *color*, *font size* in *font type*.</p>"
-	},
-	{
-		"el": ".main--header2",
-		"title" : "Header 2",
-		"comment": "<p>The header 2 is the second biggest header used on the main content of the page. It should be *color*, *font size* in *font type*.</p>"
-	},
-	{
-		"el": ".main--content",
-		"title" : "Content text",
-		"comment": "<p>The content text should be *color*, *font size* in *font type*. The line height should be *line height*.</p>"
-	},
-	{
-		"el": ".sign-up--button",
+		"el": ".hero__button",
 		"title" : "Sign up button",
 		"comment": "<p>The sign up button should be a rounded, *red* button with a plenty of padding around the text.</p>"
 	},
 	{
-		"el": ".nav--button",
-		"title" : "Navigation button",
+		"el": ".page__title",
+		"title" : "Page title",
+		"comment": "<p>The page title is located at the top left corner to clearly indicate which page the viewer is on. The title should match the navigation naming, i.e. the tile of the page that you land when you click the \"Stuff\" should be \"Stuff.\"</p>"
+	},
+	{
+		"el": ".page__description",
+		"title" : "Page description",
+		"comment": "<p>The page description should be in *gray*, *font size* in *font name*. It should be between *30 words* to *50 words*.</p>"
+	},
+	{
+		"el": ".content__title--level1",
+		"title" : "Class title",
+		"comment": "<p>The header 1 is the biggest header used on the main content of the page. It should be *color*, *font size* in *font type*.</p>"
+	},
+	{
+		"el": ".content__title--level2",
+		"title" : "Header 2",
+		"comment": "<p>The header 2 is the second biggest header used on the main content of the page. It should be *color*, *font size* in *font type*.</p>"
+	},
+	{
+		"el": ".content__tagline",
+		"title" : "Tagline for title level 2",
+		"comment": "<p>The header 2 is the second biggest header used on the main content of the page. It should be *color*, *font size* in *font type*.</p>"
+	},
+	{
+		"el": ".content__description",
+		"title" : "class__description",
+		"comment": "<p>The content text should be *color*, *font size* in *font type*. The line height should be *line height*.</p>"
+	},
+	{
+		"el": ".content__button",
+		"title" : "Class button",
 		"comment": "<p>The navigation button has slightly rounded corners. It should be in *red* and when it's hovered, it changes to a darker *red*.</p>"
 	},
 	{
-		"el": ".footer__bg",
-		"title" : "Footer background",
-		"comment": "<p>The fotter background should be *color, etc.*.</p>"
+		"el": ".class__image",
+		"title" : "Class image",
+		"comment": "<p>The navigation button has slightly rounded corners. It should be in *red* and when it's hovered, it changes to a darker *red*.</p>"
 	},
 	{
-		"el": ".footer--header",
+		"el": ".signUp__button",
+		"title" : "Sign up button",
+		"comment": "<p>The sign up button should be a rounded, *red* button with a plenty of padding around the text.</p>"
+	},
+	{
+		"el": ".footer__header",
 		"title" : "Header used in the footer",
 		"comment": "<p>Headers used in the footer should be in *gray*, *font size* in *font name*. The line-height should be *line_height*.</p>"
 	},
 	{
+		"el": ".footer__description",
+		"title" : "text paragraph in the footer",
+		"comment": "<p>text paragraph used in the footer should be in *gray*, *font size* in *font name*. The line-height should be *line_height*.</p>"
+	},
+	{
 		"el": ".form__input--box",
 		"title" : "Input box",
-		"comment": "<p>The input box of a form should have *padding*/*margin*, and has a *border* around it. </p>"
+		"comment": "<p>The input box of a form should have *padding*/*margin*, and has a *border* around it. The placeholder goes inside of the box and should indicate the type of data to input.</p>"
 	},
 	{
-		"el": ".form__input--name",
-		"title" : "Input name",
-		"comment": "<p>The form input name should be placed *here* of the form. It should be *color*, *font size* in *font type*. The line-height should be *line height*. </p>"
-	},
-	{
-		"el": ".form__input--name",
-		"title" : "Inputfield name",
-		"comment": "<p>The form input name should be placed *here* of the form. It should be *color*, *font size* in *font type*. The line-height should be *line height*. </p>"
-	},
-	{
-		"el": ".form__input--button",
+		"el": ".form__submit",
 		"title" : "Form button",
-		"comment": "<p>The form button should have *radius* border radius. IT should be *color*. </p>"
+		"comment": "<p>The form button should have *radius* border radius. IT should be *color*. The text should be *size*. </p>"
 	},
 	{
-		"el": ".form__input--button--text",
-		"title" : "Text of a form button",
-		"comment": "<p>The text used on a button should be *size* in *font style*. </p>"
+		"el": ".map",
+		"title" : "Map",
+		"comment": "<p>The map should be *size*. </p>"
 	},
-	{
-		"el": ".footer--bg",
-		"title" : "Footer background",
-		"comment": "<p>The fotter background should be *color, etc.*.</p>"
-	}
 ]
 };
